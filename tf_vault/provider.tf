1b6e159f693694949507c29f673950f610e687b2
Cristian Ramirez <cristian.ramirez@shutterfly.com>
2020-03-04 15:37:17 -0600

    removing variables.tf

--- tf_vault/provider.tf
+++ tf_vault/provider.tf
@@ -1,3 +1,10 @@
+variable "vault_address" {
+  default = "https://vault.douvault.com:8200"
+}
+variable "skip_tls_verify" {
+  default = false
+}
+
 provider "vault" {
   address         = var.vault_address
   skip_tls_verify = var.skip_tls_verify
--- tf_vault/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "vault_address" {
-  default = "https://vault.douvault.com:8200"
-}
-variable "skip_tls_verify" {
-  default = false
-}
