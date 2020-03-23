0c094c8cb0206b86b278d434ceed21a82ecd2227
bernardogza <bernardo.garza@digitalonus.com>
2020-03-23 14:44:47 -0600

    aws

--- tf_vault/secrets/aws.tf
+++ tf_vault/secrets/aws.tf
@@ -5,16 +5,17 @@ variable "aws_roles" {}
 
 resource "vault_aws_secret_backend" "aws" {
   count = var.enable_aws_dynamic_secret ? 1 : 0
+
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region     = "us-east-1"
+  region     = var.aws_region
 }
 
 resource "vault_aws_secret_backend_role" "role" {
-  count = var.enable_aws_dynamic_secret ? length(var.aws_roles) : 0 
-  backend = vault_aws_secret_backend.aws[0].path
+  count = var.enable_aws_dynamic_secret ? length(var.aws_roles) : 0
+
+  backend = vault_aws_secret_ backend.aws[0].path
   name    = var.aws_roles[count.index].name
   credential_type = "iam_user"
   policy_document = file(var.aws_roles[count.index].role)
 }
-
