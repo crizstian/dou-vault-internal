d4fe3a5ff36ed38946c7ef3be8ff2355964724fc
Cristian Ramirez <cristian.ramirez@shutterfly.com>
2020-04-01 21:39:42 -0600

    adding gitlab-ci

---
+++ tf_vault/policies/gitlabci.tf
@@ -0,0 +1,46 @@
+variable "enable_provisioner_policy" {
+  default = false
+}
+
+resource "vault_policy" "provisioner" {
+  count = var.enable_provisioner_policy ? 1 : 0
+
+  name   = "gitlab-ci"
+  policy = <<EOT
+    # Manage auth methods broadly across Vault
+    path "auth/*"
+    {
+      capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+    }
+
+    # Create, update, and delete auth methods
+    path "sys/auth/*"
+    {
+      capabilities = ["create", "update", "delete", "sudo"]
+    }
+
+    # List auth methods
+    path "sys/auth"
+    {
+      capabilities = ["read"]
+    }
+
+    # List existing policies
+    path "sys/policies/acl"
+    {
+      capabilities = ["list"]
+    }
+
+    # Create and manage ACL policies via API & UI
+    path "sys/policies/acl/*"
+    {
+      capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+    }
+
+    # List, create, update, and delete key/value secrets
+    path "secret/*"
+    {
+      capabilities = ["create", "read", "update", "delete", "list"]
+    }
+  EOT
+}
