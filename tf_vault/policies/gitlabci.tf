3330b853add9896f97f4c9fa01709cd8ea2baa0f
Cristian Ramirez <cristian.ramirez@shutterfly.com>
2020-04-02 12:48:59 -0600

    adding gitlab-ci

--- tf_vault/policies/gitlabci.tf
+++ tf_vault/policies/gitlabci.tf
@@ -12,35 +12,58 @@ resource "vault_policy" "provisioner" {
     {
       capabilities = ["create", "read", "update", "delete", "list", "sudo"]
     }
-
     # Create, update, and delete auth methods
     path "sys/auth/*"
     {
       capabilities = ["create", "update", "delete", "sudo"]
     }
-
     # List auth methods
     path "sys/auth"
     {
       capabilities = ["read"]
     }
-
     # List existing policies
     path "sys/policies/acl"
     {
       capabilities = ["list"]
     }
-
-    # Create and manage ACL policies via API & UI
+    # Create and manage ACL policies
     path "sys/policies/acl/*"
     {
       capabilities = ["create", "read", "update", "delete", "list", "sudo"]
     }
-
     # List, create, update, and delete key/value secrets
     path "secret/*"
     {
-      capabilities = ["create", "read", "update", "delete", "list"]
+      capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+    }
+    # Manage secrets engines
+    path "sys/mounts/*"
+    {
+      capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+    }
+    # List existing secrets engines.
+    path "sys/mounts"
+    {
+      capabilities = ["read"]
+    }
+    # Transit Keys
+    path "transit/keys/*"
+    {
+      capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+    }
+    # Group member can update the group information
+    path "identity/*" {
+      capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+    }
+    path "aws/*" {
+      capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+    }
+    path "gcp/*" {
+      capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+    }
+    path "azure/*" {
+      capabilities = ["create", "read", "update", "delete", "list", "sudo"]
     }
   EOT
 }
